8dfd2841f8df04295f8895b4e7a6a274deaac5e0
Bhuvi C <bhuvicbhuvic30@gmail.com>
2026-03-02 21:57:49 +0530

    Add PasswordUtil class for password hashing

---
+++ Elderly-Helping-System/src/com/elder/util/PasswordUtil.java
@@ -0,0 +1,17 @@
+package com.elder.util;
+
+import java.security.MessageDigest;
+
+public class PasswordUtil {
+
+    public static String hashPassword(String password) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(password.getBytes("UTF-8"));
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hash) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
